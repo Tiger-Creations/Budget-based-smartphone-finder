702223b9616c319480d48f15a18d5f1ebdea487a
いちご <kundu.aarav2308@gmail.com>
2026-03-31 14:08:06 +0530

    Add files via upload

---
+++ phones.py
@@ -0,0 +1,28 @@
+a=input("Enter the price range")
+if a=="Under $100":
+    print("Redmi 12")
+elif a=="$100-$200":
+    print("Motorola Moto G 5G")
+elif a=="$200-$300":
+    print("Google Pixel 6A")
+elif a=="$300-$400":
+    print("Nothing Phone(2a)")
+elif a=="$400-$500":
+    print("Iphone SE")
+elif a=="$500-$600":
+    print("Samsung Galaxy S22 Ultra")
+elif a=="$600-$700":
+    print("Samsung Galaxy S23")
+elif a=="$700-$800":
+    print("Iphone 14")
+elif a=="$800-$900":
+    print("Samsung Galaxy ZFlip 5")
+elif a=="$900-$1000":
+    print("Google Pixel 9")
+elif a=="$1000-$1100":
+    print("Samsung Galaxy S25")
+elif a=="Under $50":
+    print("Cellular phone from Nokia or Motorola")
+else:
+    print("24-Carat Solid Gold Plated IPhone 17 Pro Max")
+    
