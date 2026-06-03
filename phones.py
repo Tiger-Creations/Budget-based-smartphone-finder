82aeed59732797d4e53af397f4fd877a6eae852e
アーラヴ <kundu.aarav2308@gmail.com>
2026-06-03 19:47:58 +0530

    Refactor price range checks for phone suggestions

--- phones.py
+++ phones.py
@@ -1,26 +1,26 @@
 a=input("Enter the price range")
-if a=="Under $100":
+if a<=100 and a>200:
     print("Redmi 12")
-elif a=="$100-$200":
+elif a<=200 and a>300:
     print("Motorola Moto G 5G")
-elif a=="$200-$300":
+elif a<=300 and a>400:
     print("Google Pixel 6A")
-elif a=="$300-$400":
+elif a<=400 and a>500:
     print("Nothing Phone(2a)")
-elif a=="$400-$500":
+elif a<=500 and a>600:
     print("Iphone SE")
-elif a=="$500-$600":
+elif a<=600 and a>700:
     print("Samsung Galaxy S22 Ultra")
-elif a=="$600-$700":
+elif a<=700 and a>800:
     print("Samsung Galaxy S23")
-elif a=="$700-$800":
+elif a<=800 and a>900:
     print("Iphone 14")
-elif a=="$800-$900":
-    print("Samsung Galaxy ZFlip 5")
-elif a=="$900-$1000":
-    print("Google Pixel 9")
-elif a=="$1000-$1100":
-    print("Samsung Galaxy S25")
+elif a<=900 and a>1000:
+    print("Samsung Galaxy ZFlip 7")
+elif a<=1000 and a>1100":
+    print("Google Pixel 10")
+elif a<=1100 and a>1200":
+    print("Samsung Galaxy S26")
 elif a=="Under $50":
     print("Cellular phone from Nokia or Motorola")
 else:
