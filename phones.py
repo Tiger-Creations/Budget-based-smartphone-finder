d3ff903a0b885b50ef100d4954e55dc6b4244db7
アーラヴ <kundu.aarav2308@gmail.com>
2026-06-03 19:51:22 +0530

    Fix price range conditions in phones.py

--- phones.py
+++ phones.py
@@ -1,25 +1,25 @@
 a=input("Enter the price range")
-if a<=100 and a>200:
+if a<=100 or a>200:
     print("Redmi 12")
-elif a<=200 and a>300:
+elif a<=200 or a>300:
     print("Motorola Moto G 5G")
-elif a<=300 and a>400:
+elif a<=300 or a>400:
     print("Google Pixel 6A")
-elif a<=400 and a>500:
+elif a<=400 or a>500:
     print("Nothing Phone(2a)")
-elif a<=500 and a>600:
+elif a<=500 or a>600:
     print("Iphone SE")
-elif a<=600 and a>700:
+elif a<=600 or a>700:
     print("Samsung Galaxy S22 Ultra")
-elif a<=700 and a>800:
+elif a<=700 or a>800:
     print("Samsung Galaxy S23")
-elif a<=800 and a>900:
+elif a<=800 or a>900:
     print("Iphone 14")
-elif a<=900 and a>1000:
+elif a<=900 or a>1000:
     print("Samsung Galaxy ZFlip 7")
-elif a<=1000 and a>1100":
+elif a<=1000 or a>1100":
     print("Google Pixel 10")
-elif a<=1100 and a>1200:
+elif a<=1100 or a>1200:
     print("Samsung Galaxy S26")
 elif a=="Under $50":
     print("Cellular phone from Nokia or Motorola")
